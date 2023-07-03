213d3f5595482a10a1dd32df6e851b98a4f26609
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-03 11:08:50 +0200

    add index controller

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,1 +1,81 @@
-<h1>mammt</h1>
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid px-4">
+        <h1 class="mt-4 text-center">I tuoi Prodotti</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item active">Prodotti</li>
+        </ol>
+        <div class="card text-bg-dark mb-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <div><i class="fa-solid fa-people-group me-1"></i>Prodotti</div>
+                <a class="btn btn-primary fw-medium d-flex align-items-center" href="{{ route('admin.products.create') }}">
+                    <i class="fa-regular fa-plus me-1 text-secondary fs-5 vertical-center fw-bolder"></i>Aggiungi
+                </a>
+            </div>
+            <div class="card-body">
+                <table class="table table-dark table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col w-50">Nome</th>
+                            <th class="d-none d-lg-table-cell w-50" scope="col">Immagine</th>
+                            <th class="d-none d-sm-table-cell w-100" scope="col">Descrizione</th>
+
+                            <th class="text-center" scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                            <tr class="align-middle">
+                                {{-- Name --}}
+                                <th scope="row">
+                                    <a class="h5 w-75"
+                                        href="{{ route('admin.products.show', $product) }}">{{ $product->name }}
+                                    </a>
+                                </th>
+                                {{-- Image --}}
+                                <td class="d-none d-lg-table-cell">
+                                    <img src="{{ $product->image }}" class="d-block img-preview" alt="{{ $product->name }}"
+                                        width="100%">
+                                </td>
+                                {{-- Description --}}
+                                <td class="d-none d-sm-table-cell w-100">
+                                    {{ $product->description }}
+                                </td>
+
+                                {{-- Action Button --}}
+                                <td>
+                                    <div
+                                        class="d-flex gap-2 flex-wrap justify-content-center text-center align-items-center">
+
+                                        <a class="btn btn-success bg-gradient"
+                                            href="{{ route('admin.products.show', $product->slug) }}">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                        <a class="btn btn-primary"
+                                            href="{{ route('admin.products.edit', $product->slug) }}">
+                                            <i class="fa-solid fa-pencil"></i>
+                                        </a>
+                                        <form class="m-0 p-0 d-inline-block"
+                                            action="{{ route('admin.products.destroy', $product->slug) }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-secondary delete-button"
+                                                data-item-title="{{ $product->name }}" type="submit">
+                                                <i class="fa-solid fa-eraser"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $products->links('vendor.pagination.bootstrap-5') }}
+            </div>
+        </div>
+    </div>
+
+    @include('partials.delete-modal')
+@endsection
