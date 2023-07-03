04396fb94164e77cb15598af4a298750923fd080
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-07-03 16:45:11 +0200

    make index type & controller

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -6,23 +6,26 @@ use App\Models\Type;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTypeRequest;
 use App\Http\Requests\UpdateTypeRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TypeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+
      */
     public function index()
     {
-        //
+        $user_id = Auth::id();
+        $types = Type::all();
+        return view('admin.types.index', compact('types', 'user_id'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+
      */
     public function create()
     {
@@ -33,7 +36,7 @@ class TypeController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreTypeRequest  $request
-     * @return \Illuminate\Http\Response
+
      */
     public function store(StoreTypeRequest $request)
     {
@@ -44,18 +47,18 @@ class TypeController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
+
      */
     public function show(Type $type)
     {
-        //
+        return view('admin.types.show', compact('type'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
+
      */
     public function edit(Type $type)
     {
@@ -67,7 +70,7 @@ class TypeController extends Controller
      *
      * @param  \App\Http\Requests\UpdateTypeRequest  $request
      * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
+
      */
     public function update(UpdateTypeRequest $request, Type $type)
     {
@@ -78,7 +81,7 @@ class TypeController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
+
      */
     public function destroy(Type $type)
     {
