71f0496114b65d702edc244fd8fd414865e1b0db
Dan848 <daniele.ceravolo15@gmail.com>
2023-07-05 10:37:35 +0200

    add api type contorller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/types', [TypeController::class, 'index']);
+Route::get('/types/{slug}', [TypeController::class, 'show']);
