119231f9bbcad52fe18db336990a317a428221d9
Dan848 <daniele.ceravolo15@gmail.com>
2023-07-13 01:53:03 +0200

    pagination

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -17,9 +17,9 @@ class RestaurantController extends Controller
             $restaurants = Restaurant::with('types')
             ->whereHas('types', function ($query) use ($types_ids) {
                 $query->whereIn('types.id', $types_ids);
-            }, '=', count($types_ids))->paginate(3);
+            }, '=', count($types_ids))->paginate(12);
         } else {
-            $restaurants = Restaurant::with('types')->paginate(3);
+            $restaurants = Restaurant::with('types')->paginate(12);
         }
         return response()->json([
             'success' => true,
