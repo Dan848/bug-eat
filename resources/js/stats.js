9876bc866542b9a7d5a01002241939d4c465f797
NelloRossi <rossianiello97@gmail.com>
2023-07-14 20:05:01 +0200

    fix charts

--- resources/js/stats.js
+++ resources/js/stats.js
@@ -4,24 +4,26 @@ const chartOrders = document.getElementById('myOrders');
 const chartRestaurants = document.getElementById('myRestaurants');
 const chartEarns = document.getElementById('myEarns');
 
+const arrayData = [];
+const arrayCount = [];
+const arrayPrice = [];
+
 const orders = document.querySelectorAll('.ordini');
 orders.forEach((order) => {
-    const dataOrders = order.getAttribute('data-item-date');
-    const countOrders = order.getAttribute('data-item-count');
-    const priceOrders = order.getAttribute('data-item-price');
-    console.log(dataOrders);
-})
-
+    arrayData.push(order.getAttribute('data-item-date'));
+    arrayCount.push(order.getAttribute('data-item-count'));
+    arrayPrice.push(order.getAttribute('data-item-price'));
+});
 
 if (chartOrders) {
         // Utilizza i dati ottenuti come valori per il grafico
-        new Chart(chartOrders, {
-          type: 'bar',
-          data: {
-            labels: [],
+    new Chart(chartOrders, {
+        type: 'bar',
+        data: {
+            labels: arrayData,
             datasets: [{
               label: 'Ordini',
-              data: [1,2,3,2],
+              data: arrayCount,
               borderWidth: 1
             }]
           },
@@ -33,6 +35,28 @@ if (chartOrders) {
             }
           }
         });
+}
+
+
+if (chartEarns) {
+    new Chart(chartEarns, {
+        type: 'line',
+        data: {
+            labels: arrayData,
+            datasets: [{
+                label: 'Ristorante 1',
+                data: arrayPrice,
+                borderWidth: 1
+            },]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
 
 if (chartRestaurants) {
@@ -47,31 +71,6 @@ if (chartRestaurants) {
             }]
         },
 
-    });
-}
-if (chartEarns) {
-    new Chart(chartEarns, {
-        type: 'line',
-        data: {
-            labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-            datasets: [{
-                label: 'Ristorante 1',
-                data: [12000, 25000, 1000, 2500, 3000, 600, 15000, 21000, 35000, 4100, 2600, 9000],
-                borderWidth: 1
-            },
-            {
-                label: 'Ristorante 2',
-                data: [2000, 5000, 1000, 2500, 300, 600, 1500, 2100, 3500, 410, 2600, 9000],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
     });
 }
 
