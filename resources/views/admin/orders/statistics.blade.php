d3e0d27ed16668ad61d006568a559bfe4fb11c12
Dan848 <daniele.ceravolo15@gmail.com>
2023-07-16 17:25:47 +0200

    fixed graph col

--- resources/views/admin/orders/statistics.blade.php
+++ resources/views/admin/orders/statistics.blade.php
@@ -3,27 +3,29 @@
 @section('content')
     @if ($orders->count())
         <div class="container">
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-5">
+                <h1 class="text-center">Statistiche Ordini</h1>
                 <div class="col-12 col-lg-10 my-5">
-                    <h3>Ordini per mese</h3>
+                    <h3 class="text-center">Ordini per mese</h3>
                     <canvas id="myOrders"></canvas>
                 </div>
-                <div class="col-12 col-lg-6 my-5">
+                <div class="col-12 col-sm-12 col-md-8 my-5">
                     <h3>Incassi mensili</h3>
                     <canvas id="myEarns"></canvas>
                 </div>
-                <div class="col-8 col-lg-4  my-5">
-                    <h3 class="text-center">Ordini totali per ristorante</h3>
+                <div class="col-10 col-sm-8 col-md-4 my-5">
+                    <h3 class="text-center mb-4">Ordini totali per ristorante</h3>
                     <canvas id="myRestaurants"></canvas>
                 </div>
             </div>
+            {{-- SEND DATA TO JS --}}
             @foreach ($orders as $order)
                 <div class="ordini" data-item-date="{{ $order->month }}" data-item-count="{{ $order->total }}"
                     data-item-price="{{ $order->price }}" data-item-restaurant="{{ $order->restaurant_name }}">
-
                 </div>
             @endforeach
         @else
+            {{-- Modal No Orders --}}
             <div class="container-fluid px-4">
                 <h1 class="mt-4 text-center">Ordini</h1>
                 <ol class="breadcrumb mb-4">
