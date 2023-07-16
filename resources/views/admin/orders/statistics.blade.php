d88fe6d103a6772271ccdcfc56a70293b73bbc01
NelloRossi <rossianiello97@gmail.com>
2023-07-16 16:23:43 +0200

    fixed charts

--- resources/views/admin/orders/statistics.blade.php
+++ resources/views/admin/orders/statistics.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-12 my-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10 my-5">
                 <h3>Ordini per mese</h3>
                 <canvas id="myOrders"></canvas>
             </div>
-            <div class="col-6 my-5">
+            <div class="col-12 col-lg-6 my-5">
                 <h3>Incassi mensili</h3>
                 <canvas id="myEarns"></canvas>
             </div>
-            <div class="col-4 my-5">
-                <h3>Ordini totali per ristorante</h3>
+            <div class="col-8 col-lg-4  my-5">
+                <h3 class="text-center">Ordini totali per ristorante</h3>
                 <canvas id="myRestaurants"></canvas>
             </div>
         </div>
