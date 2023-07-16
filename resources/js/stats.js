1681c6694610ebc351e63a9721f34c579968c330
NelloRossi <rossianiello97@gmail.com>
2023-07-16 12:24:36 +0200

    fix charts

--- resources/js/stats.js
+++ resources/js/stats.js
@@ -4,9 +4,6 @@ const chartRestaurants = document.getElementById('myRestaurants');
 const chartEarns = document.getElementById('myEarns');
 
 const arrayData = [];
-const arrayCount = [];
-const arrayPrice = [];
-const restaurantName = [];
 
 const orders = document.querySelectorAll('.ordini');
 for (let i = 0; i < 12; i++){
@@ -17,7 +14,6 @@ let restaurants = [];
 let index = 0;
 orders.forEach((order) => {
     const rest_name = order.getAttribute('data-item-restaurant');
-    const rest_index = restaurants.findIndex(restaurant => restaurant.name == rest_name);
     if (!restaurants.some(restaurant => restaurant.name == rest_name)) {
         let newRestaurant = {
             name: '',
@@ -35,9 +31,7 @@ orders.forEach((order) => {
     }
 });
 
-// restaurants.ordersCount = arrayCount.push(parseInt(order.getAttribute('data-item-count'), 10));
-// arrayPrice.push(parseFloat(order.getAttribute('data-item-price')));
-// restaurantName.push(order.getAttribute('data-item-restaurant'));
+
 
 if (chartOrders) {
     // Utilizza i dati ottenuti come valori per il grafico
@@ -45,18 +39,11 @@ if (chartOrders) {
         type: 'bar',
         data: {
             labels: arrayData,
-            datasets: [
-                {
-                label: restaurants[0].name,
-                data: restaurants[0].ordersCount,
+            datasets: restaurants.map((restaurant) => ({
+                label: restaurant.name,
+                data: restaurant.ordersCount,
                 borderWidth: 1
-                },
-                {
-                label: restaurants[1].name,
-                data: restaurants[1].ordersCount,
-                borderWidth: 1
-                },
-            ]
+            })),
           },
           options: {
             scales: {
@@ -74,18 +61,11 @@ if (chartEarns) {
         type: 'line',
         data: {
             labels: arrayData,
-            datasets: [
-                {
-                    label: restaurants[0].name,
-                    data: restaurants[0].totalPrice,
-                    borderWidth: 1
-                },
-                {
-                    label: restaurants[1].name,
-                    data: restaurants[1].totalPrice,
-                    borderWidth: 1
-                },
-            ]
+            datasets: restaurants.map((restaurant) => ({
+                label: restaurant.name,
+                data: restaurant.totalPrice,
+                borderWidth: 1
+            })),
         },
         options: {
             scales: {
@@ -98,27 +78,28 @@ if (chartEarns) {
 }
 
 const totalOrdersCount = [];
-for (let i = 0; i < restaurants.length; i++){
+const restaurantNames = [];
+
+for (let i = 0; i < restaurants.length; i++) {
     let sum = 0;
-    for (let j = 0; j < restaurants[i].ordersCount.length; j++){
+    for (let j = 0; j < restaurants[i].ordersCount.length; j++) {
         sum += restaurants[i].ordersCount[j];
-        console.log('secondo for');
     }
     totalOrdersCount.push(sum);
+    restaurantNames.push(restaurants[i].name);
 }
-console.log(totalOrdersCount);
+
 if (chartRestaurants) {
     new Chart(chartRestaurants, {
         type: 'doughnut',
         data: {
-            labels: [restaurants[0].name, restaurants[1].name],
+            labels: restaurantNames,
             datasets: [{
                 label: 'Numero ordini',
                 data: totalOrdersCount,
                 borderWidth: 1
             }]
         },
-
     });
 }
 
