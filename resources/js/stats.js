fcfed04291f3c856cd5049531470edd42bed26a4
NelloRossi <rossianiello97@gmail.com>
2023-07-16 05:24:30 +0200

    fixed stats chart

--- resources/js/stats.js
+++ resources/js/stats.js
@@ -6,30 +6,57 @@ const chartEarns = document.getElementById('myEarns');
 const arrayData = [];
 const arrayCount = [];
 const arrayPrice = [];
-let totalOrders = 0;
+const restaurantName = [];
 
 const orders = document.querySelectorAll('.ordini');
-orders.forEach((order) => {
-    arrayData.push(order.getAttribute('data-item-date'));
-    arrayCount.push(order.getAttribute('data-item-count'));
-    arrayPrice.push(order.getAttribute('data-item-price'));
-});
-
-for (let i = 0; i < arrayCount; i++){
-    totalOrders = totalOrders + arrayCount[i];
+for (let i = 0; i < 12; i++){
+    arrayData.push(orders[i].getAttribute('data-item-date'))
 }
 
+let restaurants = [];
+let index = 0;
+orders.forEach((order) => {
+    const rest_name = order.getAttribute('data-item-restaurant');
+    const rest_index = restaurants.findIndex(restaurant => restaurant.name == rest_name);
+    if (!restaurants.some(restaurant => restaurant.name == rest_name)) {
+        let newRestaurant = {
+            name: '',
+            ordersCount: [],
+            totalPrice: []
+        }
+        newRestaurant.name = rest_name;
+        newRestaurant.ordersCount.push(parseInt(order.getAttribute('data-item-count'), 10));
+        newRestaurant.totalPrice.push(parseFloat(order.getAttribute('data-item-price')));
+        restaurants.push(newRestaurant);
+        index++;
+    } else {
+        restaurants[index-1].ordersCount.push(parseInt(order.getAttribute('data-item-count'), 10));
+        restaurants[index-1].totalPrice.push(parseFloat(order.getAttribute('data-item-price')));
+    }
+});
+
+// restaurants.ordersCount = arrayCount.push(parseInt(order.getAttribute('data-item-count'), 10));
+// arrayPrice.push(parseFloat(order.getAttribute('data-item-price')));
+// restaurantName.push(order.getAttribute('data-item-restaurant'));
+
 if (chartOrders) {
-        // Utilizza i dati ottenuti come valori per il grafico
+    // Utilizza i dati ottenuti come valori per il grafico
     new Chart(chartOrders, {
         type: 'bar',
         data: {
             labels: arrayData,
-            datasets: [{
-              label: 'Ordini',
-              data: arrayCount,
-              borderWidth: 1
-            }]
+            datasets: [
+                {
+                label: restaurants[0].name,
+                data: restaurants[0].ordersCount,
+                borderWidth: 1
+                },
+                {
+                label: restaurants[1].name,
+                data: restaurants[1].ordersCount,
+                borderWidth: 1
+                },
+            ]
           },
           options: {
             scales: {
@@ -47,11 +74,18 @@ if (chartEarns) {
         type: 'line',
         data: {
             labels: arrayData,
-            datasets: [{
-                label: 'Ristorante 1',
-                data: arrayPrice,
-                borderWidth: 1
-            },]
+            datasets: [
+                {
+                    label: restaurants[0].name,
+                    data: restaurants[0].totalPrice,
+                    borderWidth: 1
+                },
+                {
+                    label: restaurants[1].name,
+                    data: restaurants[1].totalPrice,
+                    borderWidth: 1
+                },
+            ]
         },
         options: {
             scales: {
@@ -63,14 +97,24 @@ if (chartEarns) {
     });
 }
 
+const totalOrdersCount = [];
+for (let i = 0; i < restaurants.length; i++){
+    let sum = 0;
+    for (let j = 0; j < restaurants[i].ordersCount.length; j++){
+        sum += restaurants[i].ordersCount[j];
+        console.log('secondo for');
+    }
+    totalOrdersCount.push(sum);
+}
+console.log(totalOrdersCount);
 if (chartRestaurants) {
     new Chart(chartRestaurants, {
         type: 'doughnut',
         data: {
-            labels: ['Il mio ristorante'],
+            labels: [restaurants[0].name, restaurants[1].name],
             datasets: [{
                 label: 'Numero ordini',
-                data: totalOrders,
+                data: totalOrdersCount,
                 borderWidth: 1
             }]
         },
